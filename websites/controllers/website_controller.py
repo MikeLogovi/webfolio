59aa8eb030b85a457d014a3f23731bf6a6f423a6
MikeLogovi <40324123+MikeLogovi@users.noreply.github.com>
2020-08-09 07:01:40 +0100

    Cool application

--- websites/controllers/website_controller.py
+++ websites/controllers/website_controller.py
@@ -3,10 +3,13 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from websites.models import Website
 from websites.forms import WebsiteCreationForm
+
+@login_required(login_url="/developpers/login")
 def index(request):
     websites = Website.objects.all().filter().order_by("created_at")
     return render(request,'websites/index.html',{'websites':websites})
 
+@login_required(login_url="/developpers/login")
 def show(request,slug):
     website = Website.objects.get(slug=slug)
     return render(request,'websites/show.html',{'website':website})
@@ -23,6 +26,7 @@ def create(request):
     form = WebsiteCreationForm()
     return render(request,'websites/create.html',{'form':form});  
 
+@login_required(login_url="/developpers/login")
 def edit(request,id):
     website = Website.objects.get(id=id)
     print(website)
@@ -39,6 +43,7 @@ def edit(request,id):
     form = WebsiteCreationForm(instance=website)
     return render(request,'websites/edit.html',{'form':form,'website':website}) 
 
+@login_required(login_url="/developpers/login")
 def delete(request,id):
     website=Website.objects.get(id=id)
     if not website:
